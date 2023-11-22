e6a557373112e8704b96c1d272396c40b5ee16b3
Nick T <nicholastroiano123@gmail.com>
2023-11-21 23:06:45 -0500

    more goodies

--- server/app.py
+++ server/app.py
@@ -1,10 +1,13 @@
 # app.py
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import subprocess
 import json
 import process_data
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/create', methods=['POST'])
